24443b8d421c46f205d9a7375ec63f6917b30560
hayaaah <128345001+thewokgamur@users.noreply.github.com>
2024-05-26 21:14:24 +0700

    Update header.jsx

--- src/header.jsx
+++ src/header.jsx
@@ -16,7 +16,6 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className='ctext' href="/">Home</Nav.Link>
             <Link
               to={{
                 pathname: `/upload`,
